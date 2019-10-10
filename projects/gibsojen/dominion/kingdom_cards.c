710b4005fb978900c0c5dd6b1081d85c70eec0bf
Jennifer Gibson <gibsojen@flip1.engr.oregonstate.edu>
2019-10-10 13:18:25 -0700

    Fix Tribute Card Call

--- projects/gibsojen/dominion/kingdom_cards.c
+++ projects/gibsojen/dominion/kingdom_cards.c
@@ -294,10 +294,45 @@ int tributeCard (int card, struct gameState *state, int handPos, int currentPlay
 }
 
 /*
-int mineCard (int card, int choice1, int choice2, int choice3, struct gameState *state, int handPos, int *bonus,
-     	       int currentPlayer) {
+int mineCard (int card, int choice1, int choice2, struct gameState *state, int handPos, int currentPlayer) {
 
+	int i;
+	int j;
 
+        j = state->hand[currentPlayer][choice1];  //store card we will trash
+
+        if (state->hand[currentPlayer][choice1] < copper || state->hand[currentPlayer][choice1] > gold)
+        {
+            return -1;
+        }
+
+        if (choice2 > treasure_map || choice2 < curse)
+        {
+            return -1;
+        }
+
+        if ( (getCost(state->hand[currentPlayer][choice1]) + 3) > getCost(choice2) )
+        {
+            return -1;
+        }
+
+        gainCard(choice2, state, 2, currentPlayer);
+
+        //discard card from hand
+        discardCard(handPos, currentPlayer, state, 0);
+
+        //discard trashed card
+        for (i = 0; i < state->handCount[currentPlayer]; i++)
+        {
+            if (state->hand[currentPlayer][i] == j)
+            {
+                discardCard(i, currentPlayer, state, 0);
+                break;
+            }
+        }
+
+        return 0;
 
 }
+
 */
