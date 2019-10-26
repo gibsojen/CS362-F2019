b9858629b7713d5802b2c7c5aea95b292e8de544
Jennifer Gibson <gibsojen@flip3.engr.oregonstate.edu>
2019-10-26 15:15:25 -0700

    Updated Makefile for unit test 1

---
+++ projects/gibsojen/dominion/unittest1.c
@@ -0,0 +1,12 @@
+#include "dominion.h"
+#include "dominion_helpers.h"
+#include "kingdom_cards.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main() {
+   printf("hello dude\n");
+
+   return 0;	
+}
